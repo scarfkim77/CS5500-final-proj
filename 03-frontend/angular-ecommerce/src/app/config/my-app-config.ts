e49869512535aaeb321ae535be21c34f67e2f1db
77 <ckq.0928@163.com>
2022-04-12 14:05:31 -0700

    User login/ logout, handle user by email address, order history

---
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oa4mdt3qwOd8N50t5d7',
+        issuer: 'https://dev-98057852.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
